422a9c36ee5fc686fe8bb651691b06363a27bed6
UnforeseenOcean <UnforeseenOcean@users.noreply.github.com>
2016-05-22 22:58:12 +0900

    Update delerw.cmd

--- delerw.cmd
+++ delerw.cmd
@@ -1,6 +1,2 @@
 @echo off
-:start
-::for /D /R %%c in (*overwatch*) do playwav yah.wav
-if exist %userprofile%\Desktop\*overwatch*.* (start delerwatch.cmd) else goto loop
-:loop
-goto start
+if exist %userprofile%\Desktop\*overwatch*.* (start delerwatch.cmd) else exit
